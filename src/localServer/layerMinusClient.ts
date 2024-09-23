1f22c95769f9aaac190cb23dc597f801d2c8842c
CoNET Project <info@CoNETTech.ca>
2024-09-23 14:25:04 -0700

    added layerMinusClient

---
+++ src/localServer/layerMinusClient.ts
@@ -0,0 +1,262 @@
+//	gossip for mining
+
+import {ethers} from 'ethers'
+import Colors from 'colors/safe'
+import {logger} from './logger'
+import {inspect} from 'node:util'
+import {RequestOptions, request } from 'node:http'
+import {createMessage, encrypt, enums, readKey, Key} from 'openpgp'
+import GuardianNodesV2ABI from './CGPNv7New.json'
+import {mapLimit} from 'async'
+import NodesInfo from './CONET_nodeInfo.ABI.json'
+import {getRandomValues} from 'node:crypto'
+
+const conet_rpc = 'https://rpc.conet.network'
+
+const GuardianNodesInfoV6 = '0x9e213e8B155eF24B466eFC09Bcde706ED23C537a'
+const CONET_Guardian_PlanV7 = '0x35c6f84C5337e110C9190A5efbaC8B850E960384'.toLowerCase()
+const provoder = new ethers.JsonRpcProvider(conet_rpc)
+
+const startGossip = (node: nodeInfo, POST: string, callback: (err?: string, data?: string) => void) => {
+	
+
+	const option: RequestOptions = {
+		hostname: node.domain,
+		port: 80,
+		method: 'POST',
+		protocol: 'http:',
+		headers: {
+			'Content-Type': 'application/json;charset=UTF-8'
+		},
+		path: "/post",
+	}
+
+	let first = true
+
+	const kkk = request(option, res => {
+
+		if (res.statusCode !==200) {
+			return logger(`startTestMiner got res.statusCode = [${res.statusCode}] != 200 error! restart`)
+		}
+
+		let data = ''
+		let _Time: NodeJS.Timeout
+
+		res.on ('data', _data => {
+
+			data += _data.toString()
+			
+			if (/\r\n\r\n/.test(data)) {
+				clearTimeout(_Time)
+				if (first) {
+					first = false
+				}
+				data = data.replace(/\r\n/g, '')
+				callback ('', data)
+				data = ''
+
+				_Time = setTimeout(() => {
+					logger(Colors.red(`startGossip [${node.ip_addr}] has 2 EPOCH got NONE Gossip Error! Try to restart! `))
+					kkk.destroy()
+				}, 24 * 1000)
+			}
+		})
+
+		res.once('error', err => {
+			kkk.destroy()
+			logger(Colors.red(`startGossip [${node.ip_addr}] res on ERROR! Try to restart! `), err.message)
+		})
+
+		res.once('end', () => {
+			kkk.destroy()
+			logger(Colors.red(`startGossip [${node.ip_addr}] res on END! Try to restart! `))
+		})
+		
+	})
+
+	kkk.on('error', err => {
+		
+		setTimeout(() => {
+			logger(Colors.red(`startGossip [${node.ip_addr}] requestHttps on Error! Try to restart! `), err.message)
+			startGossip (node, POST, callback)
+		}, 1000)
+		
+	})
+
+	kkk.end(POST)
+
+}
+
+interface IGossipStatus {
+	totalConnectNode: number
+	epoch: number
+	nodesWallets: Map<string, string[]>
+	totalMiners: number
+	nodeWallets: string[]
+}
+
+let gossipStatus: IGossipStatus = {
+	totalConnectNode: 0,
+	epoch: 0,
+	nodesWallets: new Map(),
+	totalMiners: 0,
+	nodeWallets: []
+}
+
+let previousGossipStatus = gossipStatus
+let managerWallet: ethers.Wallet
+
+const connectToGossipNode = async (node: nodeInfo ) => {
+	
+	const wallet = managerWallet
+	const key = Buffer.from(getRandomValues(new Uint8Array(16))).toString('base64')
+	const command = {
+		command: 'mining',
+		walletAddress: wallet.address.toLowerCase(),
+		algorithm: 'aes-256-cbc',
+		Securitykey: key,
+	}
+	
+	const message =JSON.stringify(command)
+	const signMessage = await wallet.signMessage(message)
+	const encryptObj = {
+        message: await createMessage({text: Buffer.from(JSON.stringify ({message, signMessage})).toString('base64')}),
+		encryptionKeys: await readKey({armoredKey: node.armoredPublicKey}),
+		config: { preferredCompressionAlgorithm: enums.compression.zlib } 		// compress the data with zlib
+    }
+
+	const postData = await encrypt (encryptObj)
+	logger(Colors.blue(`connectToGossipNode ${node.domain}:${node.ip_addr}`))
+	startGossip(node, JSON.stringify({data: postData}), (err, _data ) => {
+		if (!_data) {
+			return logger(Colors.magenta(`connectToGossipNode ${node.ip_addr} push ${_data} is null!`))
+		}
+		try {
+			const data = JSON.parse(_data)
+			const wallets = data.nodeWallets||[]
+			gossipStatus.nodesWallets.set(node.ip_addr, wallets)	
+		} catch (ex) {
+			logger(Colors.blue(`${node.ip_addr} => \n${_data}`))
+			logger(Colors.red(`connectToGossipNode JSON.parse(_data) Error!`))
+		}
+	})
+}
+
+
+
+const moveData = (block: number) => {
+
+	logger(Colors.magenta(`moveData doing ${block} validatorPool.get (${block-1})`))
+	let _wallets: string[] = []
+	gossipStatus.nodesWallets.forEach((v, keys) => {
+		_wallets = [..._wallets, ...v]
+	})
+
+	
+	logger(inspect(_wallets, false, 3, true))
+	let totalMiners = _wallets.length
+	previousGossipStatus.nodeWallets = _wallets
+	previousGossipStatus.totalConnectNode = previousGossipStatus.nodesWallets.size
+	previousGossipStatus.totalMiners = totalMiners
+
+	gossipStatus = {
+		epoch: block,
+		totalConnectNode: 0,
+		nodesWallets: new Map(),
+		totalMiners: 0,
+		nodeWallets: []
+	}
+
+	logger(Colors.magenta(`gossipStart sendEpoch ${block-1} totalConnectNode ${previousGossipStatus.totalConnectNode} totalMiners ${totalMiners}`))
+}
+
+let currentEpoch = 0
+const listenEpoch = async () => {
+	currentEpoch = await provoder.getBlockNumber()
+
+	provoder.on('block', block => {
+		currentEpoch = block
+		moveData(block)
+		logger(Colors.blue(`listenEpoch on [${currentEpoch}]`))
+	})
+
+	logger(Colors.blue(`listenEpoch start current = [${currentEpoch}]`))
+}
+
+interface nodeInfo {
+	region: string
+	ip_addr: string
+	armoredPublicKey: string
+	nftNumber: number
+	domain: string
+}
+
+let getAllNodesProcess = false
+let Guardian_Nodes: nodeInfo[] = []
+const maxScanNodesNumber = 20
+
+const getAllNodes = async () => {
+	if (getAllNodesProcess) {
+		return
+	}
+	getAllNodesProcess = true
+	const GuardianNodes = new ethers.Contract(CONET_Guardian_PlanV7, GuardianNodesV2ABI, provoder)
+	let scanNodes = 0
+	try {
+		const maxNodes: BigInt = await GuardianNodes.currentNodeID()
+		scanNodes = parseInt(maxNodes.toString())
+
+	} catch (ex) {
+		return logger (`getAllNodes currentNodeID Error`, ex)
+	}
+	if (!scanNodes) {
+		return logger(`getAllNodes STOP scan because scanNodes == 0`)
+	}
+
+	Guardian_Nodes = []
+
+	for (let i = 0; i < maxScanNodesNumber; i ++) {
+		
+		Guardian_Nodes.push({
+			region: '',
+			ip_addr: '',
+			armoredPublicKey: '',
+			nftNumber: 100 + i,
+			domain: ''
+		})
+	}
+	const GuardianNodesInfo = new ethers.Contract(GuardianNodesInfoV6, NodesInfo, provoder)
+
+	await mapLimit(Guardian_Nodes, 5, async (n: nodeInfo, next) => {
+		const nodeInfo = await GuardianNodesInfo.getNodeInfoById(n.nftNumber)
+		n.region = nodeInfo.regionName
+		n.ip_addr = nodeInfo.ipaddress
+		n.armoredPublicKey = Buffer.from(nodeInfo.pgp,'base64').toString()
+		const pgpKey1 = await readKey({ armoredKey: n.armoredPublicKey})
+		n.domain = pgpKey1.getKeyIDs()[1].toHex().toUpperCase() + '.conet.network'
+	})
+
+	getAllNodesProcess = false
+}
+
+const startGossipListening = () => {
+	if (!Guardian_Nodes.length) {
+		return logger(Colors.red(`startGossipListening Error! gossipNodes is null!`))
+	}
+	logger(Colors.blue(`startGossipListening gossipNodes = ${Guardian_Nodes.length}`))
+
+	Guardian_Nodes.forEach(n => {
+		connectToGossipNode(n)
+	})
+	
+}
+
+
+const start = async () => {
+	const wallet = ethers.Wallet.createRandom()
+	await getAllNodes()
+	startGossipListening()
+	listenEpoch()
+}
+
+start()
